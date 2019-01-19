0b071ce5d8b8e22f9231d5c61fc1bec72f09a4cb
Glorious Empire <tolajide74@gmail.com>
2019-01-19 11:09:27 +0100

    Submission

--- resources/views/partials/website-header.blade.php
+++ resources/views/partials/website-header.blade.php
@@ -19,29 +19,29 @@
 
     <!-- ===== Favicon & Different size apple touch icons ===== -->
     <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="57x57" href="/website/assets/images/apple-touch-icon-iphone.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/website/assets/images/apple-touch-icon-ipad.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/website/assets/images/apple-touch-icon-iphone-retina.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/website/assets/images/apple-touch-icon-ipad-retina.png">
-    <link rel="apple-touch-icon" sizes="167x167" href="/website/assets/images/apple-touch-icon-ipad-pro.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/website/assets/images/apple-touch-icon-iphone-6-plus.png">
-    <link rel="icon" sizes="192x192" href="assets/images/icon-hd.png">
-    <link rel="icon" sizes="128x128" href="assets/images/icon.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{asset('website/assets/images/apple-touch-icon-iphone.png')}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('website/assets/images/apple-touch-icon-ipad.png')}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('website/assets/images/apple-touch-icon-iphone-retina.png')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('website/assets/images/apple-touch-icon-ipad-retina.png')}}">
+    <link rel="apple-touch-icon" sizes="167x167" href="{{asset('website/assets/images/apple-touch-icon-ipad-pro.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('website/assets/images/apple-touch-icon-iphone-6-plus.png')}}">
+    <link rel="icon" sizes="192x192" href="{{asset('website/assets/images/icon-hd.png')}}">
+    <link rel="icon" sizes="128x128" href="{{asset('website/assets/images/icon.png')}}">
 
     <!-- ===== Google Fonts ===== -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
 
     <!-- ===== CSS links ===== -->
-    <link rel="stylesheet" type="text/css" href="/website/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/website/assets/revolution/css/settings.css">
-    <link rel="stylesheet" type="text/css" href="/website/assets/revolution/css/layers.css">
-    <link rel="stylesheet" type="text/css" href="/website/assets/revolution/css/navigation.css">
-    <link rel="stylesheet" type="text/css" href="/website/assets/css/magnific-popup.css">
-    <link rel="stylesheet" type="text/css" href="/website/assets/css/jquery.mmenu.css">
-    <link rel="stylesheet" type="text/css" href="/website/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/revolution/css/settings.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/revolution/css/layers.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/revolution/css/navigation.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/css/magnific-popup.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/css/jquery.mmenu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/css/owl.carousel.min.css')}}">
 
-    <link rel="stylesheet" type="text/css" href="/website/assets/css/style.css">
-    <link rel="stylesheet" type="text/css" href="/website/assets/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('website/assets/css/responsive.css')}}">
 
 </head>
 
@@ -54,34 +54,34 @@
     </div>
 
 
-    <script src="/website/assets/js/jquery-3.2.1.min.js"></script>
-    <script src="/website/assets/js/bootstrap.min.js"></script>
-    <script src="/website/assets/js/jquery.ajaxchimp.js"></script>
-    <script src="/website/assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="/website/assets/js/jquery.mmenu.js"></script>
-    <script src="/website/assets/js/jquery.inview.min.js"></script>
-    <script src="/website/assets/js/jquery.countTo.min.js"></script>
-    <script src="/website/assets/js/jquery.countdown.min.js"></script>
-    <script src="/website/assets/js/owl.carousel.min.js"></script>
-    <script src="/website/assets/js/imagesloaded.pkgd.min.js"></script>
-    <script src="/website/assets/js/isotope.pkgd.min.js"></script>
-    <script src="/website/assets/js/headroom.js"></script>
-    <script src="/website/assets/js/custom.js"></script>
+    <script src="{{asset('website/assets/js/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/jquery.ajaxchimp.js')}}"></script>
+    <script src="{{asset('website/assets/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/jquery.mmenu.js')}}"></script>
+    <script src="{{asset('website/assets/js/jquery.inview.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/jquery.countTo.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/imagesloaded.pkgd.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset('website/assets/js/headroom.js')}}"></script>
+    <script src="{{asset('website/assets/js/custom.js')}}"></script>
 
     <!-- ===== Slider Revolution core JavaScript files ===== -->
-    <script type="text/javascript" src="/website/assets/revolution/js/jquery.themepunch.tools.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/jquery.themepunch.tools.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/jquery.themepunch.revolution.min.js')}}"></script>
 
     <!-- ===== Slider Revolution extension scripts ===== -->
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-    <script type="text/javascript" src="/website/assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.actions.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.carousel.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.kenburn.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.migration.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.navigation.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.parallax.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.slideanims.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('website/assets/revolution/js/extensions/revolution.extension.video.min.js')}}"></script>
 
 </body>
 
